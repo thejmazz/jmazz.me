ee76848fc44fe25b536e402d7c31bf7e8870997b
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-10-11 13:25:06 -0400

    move sitemap.yml

--- builder/config.js
+++ builder/config.js
@@ -3,7 +3,7 @@
 const path = require('path')
 
 module.exports = {
-  sitemapLoc: path.resolve(__dirname, './sitemap.yml'),
+  sitemapLoc: path.resolve(__dirname, '../sitemap.yml'),
   postsDir: require('../src/config.js').postsDir,
   buildDir: path.resolve(__dirname, '../build'),
   baseURL: 'http://localhost:3001'
